9dc41cbeeaae2f80d55e61806cc237b488407a0f
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-07-07 17:31:31 +0100

    Implement advanced promises

--- src/sorting/insertion-sort/index.js
+++ src/sorting/insertion-sort/index.js
@@ -1,4 +1,7 @@
 /**
+ * INSERTION SORT
+ * Good for nearly sorted data
+ * Works for online data(ie inserting data at a moment's notice)
  * 
  * - Start by picking the second element in the array
  * - Compare the second element with the one before it and swap if necessary
@@ -8,12 +11,24 @@
  */
 
 const insertionSort = (arr) => {
-    for(let i=0; i<arr.length; i++){
-        let idx = i+1;
-        for(let j=0; j<arr.length; j++){
-            
+    // [3,2,3,8,10,4,5]
+    //  0,1,2,3, 4,5,6
+
+    // loop through every array element starting with idx=1
+    for(let i=1; i<arr.length; i++){// i = 4
+
+        // loop through all the array elements, making sure to end where j < i
+        for(let j=0; j<i; j++){
+            // if item arr[j] is larger than arr[i], swap them
+            if(arr[i]<arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
+
+    return arr;
 }
 
-console.log(insertionSort([1,2,3,8,100,4,5]));
+console.log(insertionSort([2,1,2000, 9,76,4, 7000]));
