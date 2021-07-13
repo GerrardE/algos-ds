6905f12b9032631b616405dd87da32d82c121736
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-07-13 23:07:57 +0100

    Implement first duplicate search

--- src/sorting/merge-sort/index.js
+++ src/sorting/merge-sort/index.js
@@ -1,5 +1,5 @@
 /**
- * 
+ * WRITE A FUNCTION TO MERGE TWO SORTED ARRAYS
  * - Create an empty array, take a look at the smallest values in each input array
  * - While there are still values we haven't looked at
  *      - If the value in the first array is smaller than the value in the second array, push the value 
@@ -10,3 +10,13 @@
  *      - Once we exhaust one array, push in all remaining values from the other array
  * 
  */
+
+const mergeArray = (arr1, arr2) => {
+    let store = [];
+    let i = 0;
+    let j = 0;
+
+    while(i<=arr1.length && j<=arr2.length){}
+}
+
+console.log(mergeArray([3,4,1],[50,20,7]));
